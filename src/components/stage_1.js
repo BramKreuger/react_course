20192b20c07ef851376475929f602257ff1627a6
BramKr <bramkreuger@gmail.com>
2022-09-28 15:18:36 +0200

    First proper commit

---
+++ src/components/stage_1.js
@@ -0,0 +1,66 @@
+import React,{ useState, useContext, useRef} from 'react';
+import { Button, Form, Alert} from 'react-bootstrap'
+import { MyContext } from '../context'; 
+
+const Stage1 = () => {
+
+    const textInput = useRef()
+    const context = useContext(MyContext)
+    const [error, setError] = useState([false, ''])
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        const value = textInput.current.value;
+        const validate = validateInput(value);
+
+        if(validate){ // Validate is true, add player
+            setError([false,''])
+            context.addPlayer(value)
+            textInput.current.value = ""
+        }
+    }
+
+    const validateInput = (value) => {
+
+        if (value === ''){ // Empty? 
+            setError([true, "Sorry, fill in the name!"])
+            return false
+        }
+        if(value.length <= 2) // Shorter then 3?
+        {
+            setError([true, "That name's too short!"])
+            return false
+        }
+        return true;
+    }
+
+    console.log(context.state)
+
+    return(
+        <>
+            <Form onSubmit={handleSubmit} className="mt-4">
+                <Form.Group>
+                    <Form.Control 
+                        type='text'
+                        placeholder='Add player name'
+                        name='player'
+                        ref={textInput}
+                        />
+                </Form.Group>
+
+                { error[0] ? // Error[0] : if the error is true. set to error[1] the message
+                    <Alert> 
+                        {error[1]} 
+                    </Alert>
+                :null}
+
+                <Button className='miami' variant='primary' type='submit'>
+                    Add player
+                </Button>
+            </Form>
+        </>
+    )
+
+}
+
+export default Stage1;
